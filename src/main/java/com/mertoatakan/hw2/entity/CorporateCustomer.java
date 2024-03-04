945958f680e787b2fd09b7df5ddb0263975a8309
mertoatakan <mertoatakan@gmail.com>
2024-03-04 17:43:23 +0300

    CorporateCustomer refactor

--- src/main/java/com/mertoatakan/hw2/entity/CorporateCustomer.java
+++ src/main/java/com/mertoatakan/hw2/entity/CorporateCustomer.java
@@ -1,7 +1,23 @@
 package com.mertoatakan.hw2.entity;
 
+import java.time.LocalDate;
+
 public class CorporateCustomer extends BaseCustomer{
-    protected CorporateCustomer(int id) {
-        super(id);
+    private String taxId;
+    private String sector;
+
+    public CorporateCustomer(String name, LocalDate registrationDate, String taxId, String sector) {
+        super(name, registrationDate);
+        this.taxId = taxId;
+        this.sector = sector;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    @Override
+    public void printCustomerDetails() {
+        System.out.println("Corporate Customer: " + this.name + ", Tax ID: " + this.taxId);
     }
 }
